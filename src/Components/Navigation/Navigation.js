c5804e6975b74951f5e85d85d88dd10abe435f22
JTReerink <jareerink@gmail.com>
2022-10-04 19:00:14 +0200

    add Dots

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -7,11 +7,11 @@ const Navigation = () => {
                 <h1 className="navigation__title">MadeByJaap</h1>
             </div>
             <div className="navigation__main">
-                <a href="" className="navigation__main--link">Home</a>
-                <a href="" className="navigation__main--link">Projecten</a>
-                <a href="" className="navigation__main--link">Over</a>
-                <a href="" className="navigation__main--link">Blog</a>
-                <a href="" className="navigation__main--link">Contact</a>
+                <a href="" className="navigation__main--link"><svg className="dots" viewBox="0 0 24 24"><circle cx="12" cy="12" r="10"></circle></svg> Home</a>
+                <a href="" className="navigation__main--link"><svg className="dots" viewBox="0 0 24 24"><circle cx="12" cy="12" r="10"></circle></svg> Projecten</a>
+                <a href="" className="navigation__main--link"><svg className="dots" viewBox="0 0 24 24"><circle cx="12" cy="12" r="10"></circle></svg> Over</a>
+                <a href="" className="navigation__main--link"><svg className="dots" viewBox="0 0 24 24"><circle cx="12" cy="12" r="10"></circle></svg> Blog</a>
+                <a href="" className="navigation__main--link"><svg className="dots" viewBox="0 0 24 24"><circle cx="12" cy="12" r="10"></circle></svg> Contact</a>
             </div>
             <div className="navigation__socialButtons">
                 <a href="mailto:jareerink@gmail.com"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg></a>

c53bde0172159e43c68df107889b8aa3da83022b
JTReerink <jareerink@gmail.com>
2022-10-25 14:45:49 +0200

    arrow fix

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
                 <h1 className="header__title">Jaap Reerink</h1>
                 <p className="header__subtitle">Future Software Developer</p>
             </div>
-            <a href="" className="header__arrow"><img src="./media/arrow-down.svg" alt="Arrow" /></a>
+            <a href="#projects" className="header__arrow"><img src="./media/arrow-down.svg" alt="Arrow" /></a>
         </header>
     )
 };

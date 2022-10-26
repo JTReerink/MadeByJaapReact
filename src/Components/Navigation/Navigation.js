0f7c19c1a5902ebf8b63806429a271d1fbd7e086
JTReerink <jareerink@gmail.com>
2022-10-26 19:11:54 +0200

    add socialbuttons animation

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -3,15 +3,22 @@ import "./Navigation.css";
 
 const Navigation = () => {
     const [hidden, setHidden] = useState(false);
+    const [scrolledSocials, setScrolledSocials] = useState(false);
+    const [hiddenSocials, setHiddenSocials] = useState(false);
 
     useEffect(() => {
-        
+
         window.addEventListener("scroll", () => {
-            if(window.scrollY > 100) setHidden(true);
+            if(window.scrollY > 1) setScrolledSocials(true);
+            else setScrolledSocials(false);
+            
+            if (window.scrollY > 100) setHidden(true);
             else setHidden(false);
+
+            if (document.documentElement.scrollHeight - window.scrollY - window.innerHeight < 300) setHiddenSocials(true);
+            else setHiddenSocials(false);
         })
     }, []);
-
 
     return (
         <nav className="navigation">
@@ -22,10 +29,11 @@ const Navigation = () => {
                 <a href="#" className="navigation__main--link"><svg className="dots" viewBox="0 0 24 24"><circle cx="12" cy="12" r="10"></circle></svg> Home</a>
                 <a href="#projects" className="navigation__main--link"><svg className="dots" viewBox="0 0 24 24"><circle cx="12" cy="12" r="10"></circle></svg> Projecten</a>
                 <a href="#about" className="navigation__main--link"><svg className="dots" viewBox="0 0 24 24"><circle cx="12" cy="12" r="10"></circle></svg> Over</a>
-                <a href="#blog" className="navigation__main--link"><svg className="dots" viewBox="0 0 24 24"><circle cx="12" cy="12" r="10"></circle></svg> Blog</a>
+                {/* <a href="#blog" className="navigation__main--link"><svg className="dots" viewBox="0 0 24 24"><circle cx="12" cy="12" r="10"></circle></svg> Blog</a> */}
                 <a href="#contact" className="navigation__main--link"><svg className="dots" viewBox="0 0 24 24"><circle cx="12" cy="12" r="10"></circle></svg> Contact</a>
             </div>
-            <div className={hidden ? "navigation__socialButtons navigation__socialButtons--hidden" : "navigation__socialButtons"}>
+            {/*             when scrolledSocials but not hiddenSocials (hiddenSocials takes precedence)                         when hiddenSocials                                                                     This is the default*/}
+            <div className={scrolledSocials && !hiddenSocials ? "navigation__socialButtons navigation__socialButtons--hidden" : hiddenSocials ? "navigation__socialButtons navigation__socialButtons--hiddenSocials" : "navigation__socialButtons"}>
                 <a href="mailto:jareerink@gmail.com"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg></a>
                 <a href="https://www.linkedin.com/in/jaapreerink/"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentcolor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg></a>
             </div>
